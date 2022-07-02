d37248f7b38bbeb8f1442924b110e5bb882cf491
Waldorfio <walid.hanifi707@gmail.com>
2022-07-02 21:56:01 +1000

    Add solution function for all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(inp) {
+    output = false;
+    if (inp%4==0) {
+        output = true;
+    }
+    if (inp%100==0) {
+      output = false;
+      if (inp%400==0) {
+        output = true;
+      }
+    }
+    return output
 };
 
 // Do not edit below this line

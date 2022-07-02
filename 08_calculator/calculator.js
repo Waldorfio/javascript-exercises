16a9f076e6a2a2d28f0999ea493a154d50c20452
Waldorfio <walid.hanifi707@gmail.com>
2022-07-02 23:34:21 +1000

    Add solution function for all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  output = 0;
+  for (i=0; i<a.length; i++) {
+    output = output + a[i];
+  }
+ 	return (output);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  output = 1;
+  for (i=0; i<a.length; i++) {
+    output = output * a[i];
+  }
+ 	return (output);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return (a**b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  output = 1;
+  for (i=1; i<a+1; i++) {
+    output = output * i;
+  }
+	return output;
 };
 
 // Do not edit below this line

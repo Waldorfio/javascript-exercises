f02c339b6977d743885c8141ceac105e35c1a46a
Waldorfio <walid.hanifi707@gmail.com>
2022-07-02 21:30:43 +1000

    Add solution function for all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+function sumAll(int1, int2) {
+    output = 0;
 
-};
+    int1New = int1;
+    int2New = int2;
+    if (int1 > int2) {
+        int1New = int2;
+        int2New = int1;
+    }
+
+    for (i=int1New; i<int2New+1; i++) {
+      output = output + i;
+    }
+
+    if (typeof(int1) != "number" || typeof(int2) != "number" || int1 < 0 || int2 < 0) {
+        output = "ERROR"
+    }
+
+    return output
+  }
 
 // Do not edit below this line
 module.exports = sumAll;

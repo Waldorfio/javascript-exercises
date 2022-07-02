3575e140d07e492b65defd0a80be8dffc4ea7a4c
Waldorfio <walid.hanifi707@gmail.com>
2022-07-02 20:58:29 +1000

    Add solution function for all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
+function reverseString(string) {
+    let output = '';
+    let j = 0;
+    for (let i=0; i<string.length; i++) {
+        j = string.length-1 - i;
+        output = output + string[j];
+    }
+    return(output); 
+}
 
 // Do not edit below this line
 module.exports = reverseString;

53e579f3f5277631cd444f8a0d4d1429c0bffb59
Waldorfio <walid.hanifi707@gmail.com>
2022-07-03 00:02:08 +1000

    Add solution function for all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    output = true;
+    a = a.replaceAll(',', '');
+    a = a.replaceAll('.', '');
+    a = a.replaceAll(' ', '');
+    a = a.replaceAll('!', '');
+    a = a.toUpperCase();
+    combinedString = a;
+    // Old attempt at checking for space between words.
+    // Later switched to using .replaceAll
+    // if (a.includes(' ')==true) {
+    //   a = a.replaceAll(',', '');
+    //   a = a.replaceAll('.', '');
+    //   combinedString = '';
+    //   splitArray = a.split(' ')
+    //   for (i=0; i<splitArray.length; i++) {
+    //       combinedString = combinedString + splitArray[i];
+    //   }
+    // }
+    for (i=0; i<combinedString.length-1; i++) {
+        if (combinedString[i] != combinedString[combinedString.length-1 - i]) {
+          output = false;
+          break;
+        }
+    }
+    return output
 };
 
 // Do not edit below this line

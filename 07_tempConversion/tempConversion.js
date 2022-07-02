d3d8a5e00b84b5ef86c5f589dbfde3900bb4901a
Waldorfio <walid.hanifi707@gmail.com>
2022-07-02 23:21:58 +1000

    Add solution function for all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fInput) {
+  output = (fInput - 32) * (5/9);
+  output = parseFloat(output.toFixed(1));
+  return output
 };
 
-const ctof = function() {
-
+const ctof = function(cInput) {
+  output2 = (cInput * 9/5) + 32;
+  output2 = parseFloat(output2.toFixed(1));
+  return output2
 };
 
 // Do not edit below this line

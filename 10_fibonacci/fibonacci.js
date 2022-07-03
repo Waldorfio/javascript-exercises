a015f5f004095a9ad622ef9cc139313e5a93f7e3
Waldorfio <walid.hanifi707@gmail.com>
2022-07-03 14:24:57 +1000

    Add solution function for all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let fibArr = [1, 1, 2];
 
-};
+    for (i=0; i<a; i++) {
+      j = parseInt(i);
+      if (j==0 || j==1 || j==2) {
+        continue
+      }
+      finalFib = fibArr[fibArr.length-1] + fibArr[fibArr.length-2];
+      fibArr.push(finalFib);
+    }
+
+    finalFib = fibArr[fibArr.length-1];
+    if (parseInt(a) < 3) {finalFib = 1};
+    if (a < 0) {finalFib = "OOPS"}
+
+    return finalFib
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;

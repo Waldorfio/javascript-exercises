ca03b9ca25f51b169ad2262f7d4ada1effb4a382
Waldorfio <walid.hanifi707@gmail.com>
2022-07-03 15:32:58 +1000

    Add solution function for all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrInput) {
+    newArr = [];
+    newArr2 = [];
+    for (i=0; i<(arrInput.length); i++) {
+        if (arrInput[i].yearOfDeath == undefined) {
+            newArr.push(2022 - arrInput[i].yearOfBirth);
+            continue
+        }
+        newArr.push(arrInput[i].yearOfDeath - arrInput[i].yearOfBirth);
+        newArr2.push(arrInput[i].name);
+    }
 
-};
+    maxVal = Math.max(...newArr);
+    maxIdx = newArr.indexOf(maxVal);
+    oldPerson = arrInput[maxIdx];
+
+    return oldPerson
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;

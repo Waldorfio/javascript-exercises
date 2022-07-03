e24218bfe93f5f2eb997414cbb70c3389846d245
Waldorfio <walid.hanifi707@gmail.com>
2022-07-03 14:33:53 +1000

    Add solution function for all tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrInput) {
+    titleArr = [];
+    for (i=0; i<(arrInput.length); i++) {
+        arrInput[i]
+        titleArr.push(arrInput[i].title);
+    }
+    return titleArr
 };
 
 // Do not edit below this line
